7475faea79c9aee203e4a90321e3ec7640a355c7
Greyden <vadercat1@icloud.com>
2023-10-22 13:10:20 -0500

    made program that determines most and least unique

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import re
 import json
+import os
+import sys
+
+def clear():
+	if sys.platform.startswith("linux"):
+		os.system("clear")
+	elif sys.platform.startswith("win32"):
+		os.system("cls")
+	elif sys.platform.startswith("darwin"):
+		os.system("clear")
 
 def get_all_lyrics():
     with open("lyrics.json", 'r') as json_file:
@@ -11,7 +21,9 @@ def remove_brackets(lyrics):
 
 def cleanse_lyrics(lyrics):
     clean_lyrics = remove_brackets(lyrics)
-    return re.sub(r'\s+', " ", clean_lyrics.replace('\n', ' ').strip())
+    clean_lyrics = re.sub(r'\s+', " ", clean_lyrics.replace('\n', ' ').strip())
+    clean_lyrics = clean_lyrics.lower()
+    return re.sub(r'\s+', " ", clean_lyrics.replace('-', ' ').strip())
 
 def count_words(lyrics):
     words = lyrics.split()
